02e23ab33bea0ffbf3c6930c4911496ba6a210fb
Aleks <samenusku@gmail.com>
2021-08-27 16:06:56 -0400

    I made a funny code
    And Or operators

---
+++ Sinon.java
@@ -0,0 +1,15 @@
+public class Sinon {
+
+    public static void main(String[] args) {
+        int Valeur = 34;
+        int Valeur2 = -23;
+        if (Valeur < 0 && Valeur2 < 0) {
+            System.out.println("Deux negative");
+        } else if (Valeur > 0 && Valeur2 > 0) {
+            System.out.println("both positive");
+        } else {
+            System.out.println("one negative one positive");
+        }
+    }
+
+}
