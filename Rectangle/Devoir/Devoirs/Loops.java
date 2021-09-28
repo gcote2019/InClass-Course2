371b4fb9d2343a3682fba3510a75be8ea86bd9dd
Aleks <samenusku@gmail.com>
2021-09-28 15:19:35 -0400

    Update Loops.java

--- Rectangle/Devoir/Devoirs/Loops.java
+++ Rectangle/Devoir/Devoirs/Loops.java
@@ -8,10 +8,10 @@ public class Loops{
     int Chiffre;
     Scanner Keyboard = new Scanner(System.in);
     public static void main(String[] args){
+        system.out.println("Mettre Un chiffre")
         while (!Keyboard.hasNextInt()>0) {
             System.out.println("Mets un autre chiffre");
             Keyboard.next();
-          
           }
           Chiffre = Keyboard.nextInt();
           System.out.println(Chiffre);
